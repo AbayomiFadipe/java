19e7955d173cf79a6ee3f536a8382b17e4822af9
AbayomiFadipe <64590066+AbayomiFadipe@users.noreply.github.com>
2021-03-26 01:47:56 +0800

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    console.log('click')
+    e.preventDefault();
+});
+
+document.querySelector('#my-form').style.background = '#F6F6F6';
+document.querySelector('.btn').style.background ='red';
+
+
+
+
+
+
+
+
+
+
+
